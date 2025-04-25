c03809fc2acccf68248fe1e1217606fd313486bc
Devang <23aiml014@charusat.edu.in>
2025-04-25 22:16:22 +0530

    ok

--- face_recognization.py
+++ face_recognization.py
@@ -1,22 +1,22 @@
 import cv2
 
-# Load the pre-trained Haar Cascade for face detection
+
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-# Read the image
-image = cv2.imread('image2.png')
 
-# Convert the image to grayscale for better accuracy in detection
+image = cv2.imread('image2.jpg')
+
+
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-# Detect faces
+
 faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-# Draw rectangles around faces
+
 for (x, y, w, h) in faces:
     cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-# Show the output
+
 cv2.imshow('Face Detection', image)
 
 cv2.waitKey(0)
